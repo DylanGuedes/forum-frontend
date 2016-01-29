ec42353b9c3de198c0bdd6d7c8a1085e42c3a533
Dylan Guedes <djmgguedes@gmail.com>
2016-01-29 20:48:07 -0200

    new posts improved

--- app/routes/section.js
+++ app/routes/section.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    return Ember.RSVP.hash({
-      section: this.store.find('section', params.section_id),
+  model: function(params) {
+    var _this = this;
+    return this.store.find('section', params.section_id).then(function(section) {
+      return Ember.RSVP.hash({
+        section: section,
+      });
     });
   }
 });
